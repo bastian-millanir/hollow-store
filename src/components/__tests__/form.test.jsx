56a458fb01c9f5ca076f3b6fb44f5ae2133d698c
CETECOM <CETECOM@AVAAOV01LC0424.duoc.acad>
2025-10-09 21:49:28 -0300

    update form.test.jsx

--- src/components/__tests__/form.test.jsx
+++ src/components/__tests__/form.test.jsx
@@ -3,17 +3,38 @@ import form from '../navbar.jsx';
 import { describe, expect } from "vitest";
 describe('Componente navbar',() => {
     it('debe contener los campos del formulario', () => {
-        render(<form/>);
+        render(<Form/>);
         const nombre = screen.getByLabelText(/Nombre/i);
         const email = screen.getByLabelText(/Email/i);
         const mensaje = screen.getByLabelText(/Mensaje/i);
-        const boton = screen.getByRole('button',{name: /Enviar/i});
-
-        fireEvent.change(nombre,{target:{value:'Juan'}});
-        fireEvent.change(nombre,{target:{value:'juantest'}});
-         
+       
         expect(nombre).toBeTruthy();
- 
-    })
+        expect(email).toBeTruthy();
+        expect(mensaje).toBeTruthy();
+         
+        
+    });
+
+    it('debe mostrar alert al enviar datos',()=>{
+        render(<Form/>);
+        vi.spyOn(window,'alert')
+        //SVGPolygonElement()
+        const nombre = screen.getByLabelText(/Nombre/i);
+        const email = screen.getByLabelText(/Email/i);
+        const mensaje = screen.getByLabelText(/Mensaje/i);
+        const boton = screen.getByText( /Enviar/i);
+        
+        fireEvent.change(nombre, {target:{value:'Juan'}});
+        fireEvent.change(email, {target:{value:'juantest'}});
+        fireEvent.change(mensaje, {target:{value:'Prueba juan'}});
+        fireEvent.click(boton);
+
+
+        expect(window.alert).toHaveBeenCalled();
+       expect(nombre.value).toBe('');
+       expect(email.value).toBe('');
+        expect(mensaje.value).toBe('');
+
+    });
 
 });
